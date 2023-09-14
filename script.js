dd7ad7a1ab1f3f437a6a5241c5b955df80e2719a
Arturo <arturodamianvargas1996@gmail.com>
2023-09-13 22:00:48 -0500

    div tag inclusions and java notes

---
+++ script.js
@@ -0,0 +1,6 @@
+// Rules for Quiz
+//1. You will have 25 seconds per question. (Include Span tag for seconds)
+//2. Once you have selected your answer it is final, there is no reselection.
+//3. Once timer goes off, no options can be selected.
+//4. No exiting the quiz while you're playing.
+//5. Each correct answer earns point.
