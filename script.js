c43bd04a85f2e9f8a2f4a160c4c73ef41218cb40
Arturo <arturodamianvargas1996@gmail.com>
2023-09-16 20:44:50 -0500

    html changes and css inclusions made

--- script.js
+++ script.js
@@ -4,3 +4,38 @@
 //3. Once timer goes off, no options can be selected.
 //4. No exiting the quiz while you're playing.
 //5. Each correct answer earns point.
+
+
+// Quiz Questions Regarding Coding (5 Questions) Keep it Simple!
+
+// What does HTML stand for?
+
+//Hyper Tax Module Language
+//Hyper Text Markup Language
+//Hyper Tool Multi Language
+//Hyper Text Processing Language
+
+
+// What does CSS stand for?
+//Cascading Style Sheets
+//Common Style Sheet
+//Computer Style Sheet
+//Color Style Sheet
+
+// What is a widely used script that adds functionality and interactivity to a webpage?
+//HTML
+//CSS
+//Javascript 
+//DOM
+
+//What does DOM stand for?
+//Document Object Modum
+//Document Orion Markup
+//Document Object Model
+//Document Over Markup
+
+// Application Programming Interfaces are also known as what?
+//APO’S
+//API’S
+//APL’S
+//APM’S
