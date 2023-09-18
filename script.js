63b7657b18b9cec83532743159b8ce6d23bbfe83
Arturo <arturodamianvargas1996@gmail.com>
2023-09-17 19:14:15 -0500

    added js code

--- script.js
+++ script.js
@@ -1,3 +1,170 @@
+//Creating an array for the numbers and questions with options (answers)//
+
+var questions = [
+    {
+        number: 1,
+        question:" What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Tax Module Language",
+            "Hyper Text Markup Language",
+            "Hyper Tool Multi Language",
+            "Hyper Text Processing Language",
+
+        ]
+    }
+
+
+
+
+
+    {
+        number: 2,
+        question:" What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Cascading Style Sheets",
+            "Common Style Sheet",
+            "Computer Style Sheet",
+            "Color Style Sheet",
+
+        ]
+    }
+
+
+
+    {
+        number: 3,
+        question:" What is a widley used script that adds functionality and interactivity to a webpage?",
+        answer: "CSS",
+        options: [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "DOM",
+
+        ]
+    }
+
+
+    {
+        number: 4,
+        question:" What does DOM stand for?",
+        answer: "Document Object Model",
+        options: [
+            "Document Object Modum",
+            "Document Orion Markup",
+            "Document Object Model",
+            "Document Over Markup",
+
+        ]
+    }
+
+
+    {
+        number: 5,
+        question:"Application Programming Interfaces are also known as what?",
+        answer: "API'S",
+        options: [
+            "APO'S",
+            "API'S",
+            "APL'S",
+            "APM'S",
+
+        ]
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Rules for Quiz
 //1. You will have 25 seconds per question. (Include Span tag for seconds)
 //2. Once you have selected your answer it is final, there is no reselection.
@@ -39,3 +206,7 @@
 //API’S
 //APL’S
 //APM’S
+
+
+
+
